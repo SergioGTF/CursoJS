5d4746a86b05bd1e48f6ee89292cca397b34b5ce
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-08-08 14:44:39 -0300

    Desenvolvimento web (Javascript, HTML, CSS)

--- Aula06/ex004.js
+++ Aula06/ex004.js
@@ -1,5 +1,5 @@
 
 var nome = window.prompt('Qual seu nome?')
         document.write(`Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras.<br>`)
-        document.write(`Seu nome em maíusculas é ${nome.toUpperCase()} <br>`)
+        document.write(`Seu nome em maíusculas é <strong>${nome.toUpperCase()}<strong> <br>`)
         document.write(`Seu nome em minúsculas é <strong>${nome.toLowerCase()}</strong> `)
