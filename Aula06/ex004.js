a0c9953afb3f6c3a71bde36fda997cbc555a07aa
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-08-14 15:19:09 -0300

    Indentação

--- Aula06/ex004.js
+++ Aula06/ex004.js
@@ -1,5 +1,10 @@
-
-var nome = window.prompt('Qual seu nome?')
-        document.write(`Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras.<br>`)
-        document.write(`Seu nome em maíusculas é <strong>${nome.toUpperCase()}<strong> <br>`)
-        document.write(`Seu nome em minúsculas é <strong>${nome.toLowerCase()}</strong> `)
+var nome = window.prompt("Qual seu nome?");
+document.write(
+  `Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras.<br>`
+);
+document.write(
+  `Seu nome em maíusculas é <strong>${nome.toUpperCase()}<strong> <br>`
+);
+document.write(
+  `Seu nome em minúsculas é <strong>${nome.toLowerCase()}</strong> `
+);
